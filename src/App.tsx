845efb74083ab94d6016a7d21b8dd8bfe1dd2c0e
Viplav jha <viplavjha1993@gmail.com>
2024-01-19 10:37:25 +0530

    react-router-dom setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,35 @@
-import { ThemeProvider, createTheme } from "@mui/material/styles"
-import { useMemo } from "react"
-import { themeSettings } from "./theme"
-import { CssBaseline } from "@mui/material"
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { useMemo } from "react";
+import { themeSettings } from "./theme";
+import { Box, CssBaseline } from "@mui/material";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "@/screen/navbar";
+import Dashboard from "@/screen/dashboard"
+
 
 function App() {
-
-   const theme =useMemo(()=> createTheme(themeSettings),[])
+  const theme = useMemo(() => createTheme(themeSettings), []);
   return (
     <>
       <div className="app">
-        <ThemeProvider theme={theme}>
-            <CssBaseline/>
-            hi
-        </ThemeProvider>
+        <BrowserRouter>
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <Box width="100%" padding="1rem 2rem 4rem 2rem">
+              <Navbar/>
+              <Routes>
+                <Route path="/" element={<Dashboard/>} />
+                <Route
+                  path="/prediction"
+                  element={<div>predication page</div>}
+                />
+              </Routes>
+            </Box>
+          </ThemeProvider>
+        </BrowserRouter>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
