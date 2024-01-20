7cb33e17d1e2a1da556f868cc838c7e056b76fda
Viplav jha <viplavjha1993@gmail.com>
2024-01-20 09:10:38 +0530

    index file commit

--- src/screen/dashboard/index.tsx
+++ src/screen/dashboard/index.tsx
@@ -1,15 +1,87 @@
-import { useTheme } from '@mui/material'
-import { Box } from '@mui/material'
+import DashboardBox from "@/components/DashboardBox";
+import { useMediaQuery } from "@mui/material";
+import { Box } from "@mui/material";
+import Row1 from "./Row1";
+import Row2 from "./Row2";
+import Row3 from "./Row3";
 
+type Props = {};
 
-type Props = {}
+const gridTemplateLargeScreen = `
+ "a b c"
+ "a b c"
+ "a b c"
+ "a b f"
+ "d e f"
+ "d e f"
+ "d h i"
+ "g h i"
+ "g h j"
+ "g h j"
+`;
 
-const Dashboard = (props: Props) => {
-    const {palette} =useTheme();
+const gridTemplateSmallScreen =`
+ "a "
+ "a "
+ "a "
+ "a "
+ "b "
+ "b "
+ "b "
+ "c "
+ "c "
+ "c "
+ "d "
+ "d "
+ "d "
+ "e "
+ "e "
+ "f "
+ "f "
+ "f "
+ "g "
+ "g "
+ "g "
+ "g "
+ "h "
+ "h "
+ "h "
+ "h "
+ "i "
+ "i "
+ "j "
+ "j "
+`
+
+const Dashboard = () => {
+const isAbovemediumScreens = useMediaQuery("(min-width:1200px)")
+
 
   return (
-    <Box color={palette.grey[300]}>Dashboard</Box>
-  )
-}
+    <Box
+      width="100%"
+      height="100%"
+      display="grid"
+      gap="1.5rem"
+      sx={
+        
+        isAbovemediumScreens ?
+        {
+        gridTemplateColumns: "repeat(3, minmax(370px, 1fr))",
+        gridTemplateRows: "repeat(10, minmax(60px, 1fr))",
+        gridTemplateAreas: gridTemplateLargeScreen,
+      } :{
+        gridAutoColumns: "1fr",
+        gridAutoRows: "80px",
+        gridTemplateAreas:gridTemplateSmallScreen,
+      }
+    }
+    >  
+      <Row1/>
+      <Row2/>
+      <Row3 />
+    </Box>
+  );
+};
 
 export default Dashboard;
