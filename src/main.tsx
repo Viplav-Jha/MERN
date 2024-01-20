1f578e604985bc05f9cb95cdb3e7a4432e37f88b
Viplav jha <viplavjha1993@gmail.com>
2024-01-20 09:10:06 +0530

    main file

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+import './index.css';
+import { configureStore } from '@reduxjs/toolkit';
+import { setupListeners } from '@reduxjs/toolkit/query';
+import {api} from '@/state/api';
+import { Provider } from 'react-redux';
 
+export const store = configureStore({
+  reducer:{[api.reducerPath]:api.reducer},
+  middleware:(getDafult)=>getDafult().concat(api.middleware)
+})
+setupListeners(store.dispatch)
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
 )
